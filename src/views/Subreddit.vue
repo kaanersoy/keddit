875b2dc40483628308029b7093fc39e3acef6e6e
Kaan Ersoy <kaanersoy12@gmail.com>
2021-03-02 21:17:09 +0300

    Fix to router

--- src/views/Subreddit.vue
+++ src/views/Subreddit.vue
@@ -1,12 +1,18 @@
 <template>
   <section class="subreddit">
-    <h1>{{ this.$route.params.subreddit }}</h1>
+    <h1>{{ subredditName }}</h1>
   </section>
 </template>
 
 <script>
 export default {
   name: 'Subreddit',
+  data: () => ({
+    subredditName: null,
+  }),
+  mounted() {
+    this.subredditName = this.$route.params.subreddit;
+  },
 };
 </script>
 
